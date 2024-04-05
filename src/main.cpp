7ef367bb5b9e4bb833a357135179dc10929b8ce9
Danny Mou <dannymou7426@hotmail.com>
2024-04-04 19:09:41 -0600

    Menus working ok

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,66 @@
 #include <iostream>
+#include <string>
+#include <vector>
 #include <stdlib.h>
-
 
 using namespace std;
 
 
-int mainMenu(){
-    int option;
-    while(option != 3){
-        cout << "================" << endl;
-        cout << " Main Menu\n1. Start Game\n2. Load Game\n3. Exit to menu" << endl;
-        cout << "================" << endl;
+void gameMenu();
+void startGame();
+void loadGame();
+
+
+void mainMenu() {
+    int option = 0; 
+
+    while (true) { 
+        cout << "=======================" << endl;
+        cout << " Welcome to Baaldur-C!\n1. Enter Game\n2. Exit Game" << endl;
+        cout << "=======================" << endl;
         cin >> option;
-        switch (option){
+
+        switch (option) {
             case 1:
-                //startGame();
+                gameMenu(); 
                 break;
             case 2:
-                //loadGame();
-                break;
-            case 3:
-                cout << "Exiting to menu" << endl;
-                break;
+                cout << "Thanks for playing! :)" << endl;
+                return; 
             default:
-                cout << "Invalid option, please choose a valid one" << endl;
-                break;
+                cout << "Invalid option, please choose a valid one." << endl;
         }
     }
 }
 
-int main(){
-    int option;
-    while (option != 2){
-        cout << "=======================" << endl;
-        cout << " Welcome to Baaldur-C!\n1. Enter\n2. Exit Game"<< endl;
-        cout << "=======================" << endl;
+
+void gameMenu() {
+    int option = 0; 
+
+    while (true) { // Main loop for the game menu
+        cout << "================" << endl;
+        cout << " Game Menu\n1. Start Game\n2. Load Game\n3. Exit to Main Menu" << endl;
+        cout << "================" << endl;
         cin >> option;
-        switch (option){
+
+        switch (option) {
             case 1:
-                mainMenu();
+                //startGame(); 
                 break;
             case 2:
-                cout << "Thanks for playing! :)" << endl;
+                //loadGame(); 
                 break;
+            case 3:
+                return; 
             default:
-                cout << "Invalid option, please choose a valid one" << endl;
-                break;
-
+                cout << "Invalid option, please choose a valid one." << endl;
         }
-
-
     }
+}
+
+
+
+int main() {
+    mainMenu();
     return 0;
 }
